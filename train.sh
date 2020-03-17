73fea26ced05d85305d1888dba1f4492eede16e1
13952522076 <xuma@my.unt.edu>
2020-03-16 22:25:06 -0500

    update run

--- train.sh
+++ train.sh
@@ -1,4 +1,11 @@
-python3 cifar.py --netName=old_resnet50
-python3 cifar.py --netName=ac_resnet50
-python3 cifar.py --netName=double_resnet50
-python3 cifar.py --netName=triple_resnet50
+#!/bin/bash
+python3 cifar.py --netName=old_resnet50 --bs=512 --cifar=100
+
+sleep 1m
+python3 cifar.py --netName=ac_resnet50 --bs=512 --cifar=100
+
+sleep 1m
+python3 cifar.py --netName=double_resnet50 --bs=512 --cifar=100
+
+sleep 1m
+python3 cifar.py --netName=triple_resnet50 --bs=512 --cifar=100
