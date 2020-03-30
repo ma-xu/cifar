5f713ae4bf07ce40c3ed3f5c2549bd8fb7bcb85f
13952522076 <xuma@my.unt.edu>
2020-03-30 10:25:36 -0500

    update

--- train3.sh
+++ train3.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-python3 cifar2.py --netName=old_resnet50 --bs=128 --cifar=100
-
-sleep 1m
 python3 cifar2.py --netName=ass1_resnet50 --bs=128 --cifar=100
 
 sleep 1m
@@ -13,5 +10,4 @@ python3 cifar2.py --netName=ass3_resnet50 --bs=128 --cifar=100
 sleep 1m
 python3 cifar2.py --netName=ass4_resnet50 --bs=128 --cifar=100
 
-sudo poweroff
 
