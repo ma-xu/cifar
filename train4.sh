5a32813852d493139846e81259ff2520a424b880
13952522076 <xuma@my.unt.edu>
2020-04-06 11:19:26 -0500

    new train

--- train4.sh
+++ train4.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-python3 cifar3.py --netName=ass6_resnet18 --bs=128 --cifar=100
+python3 cifar4.py --netName=old_resnet18 --bs=128 --cifar=100
 
 sleep 1m
-python3 cifar3.py --netName=ass7_resnet18 --bs=128 --cifar=100
+python3 cifar4.py --netName=ac_resnet18 --bs=128 --cifar=100
 
 sleep 1m
-python3 cifar3.py --netName=ass8_resnet18 --bs=128 --cifar=100
+python3 cifar4.py --netName=double_resnet18 --bs=128 --cifar=100
 
 sleep 1m
-python3 cifar3.py --netName=ass9_resnet18 --bs=128 --cifar=100
+python3 cifar4.py --netName=ass1_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass2_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass3_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass4_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass5_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass6_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass7_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass8_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=ass9_resnet18 --bs=128 --cifar=100
+
+sleep 1m
+python3 cifar4.py --netName=triple_resnet18 --bs=128 --cifar=100
+
 
 sleep 2m
 sudo poweroff
